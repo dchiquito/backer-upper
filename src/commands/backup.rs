06b83143086be83991c9801a244a9b5a5ac00aee
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-04-26 17:29:42 -0400

    Basics of backup

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -1,8 +1,51 @@
-use clap::error::Error;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
+use std::process::Command;
 
-pub fn backup(globs: &[String], output: &Option<PathBuf>) -> Result<(), Error> {
-    todo!()
+fn run(command: &mut Command) {
+    let output = command.output().unwrap();
+    if !output.stderr.is_empty() {
+        eprint!("{}", String::from_utf8(output.stderr).unwrap());
+        panic!("Error running command");
+    }
+}
+
+pub fn backup(globs: &[String], output: &Option<PathBuf>) -> Result<(), clap::error::Error> {
+    println!("{:?}", globs);
+    let output_file = output
+        .as_ref()
+        .map(|pb| {
+            pb.clone()
+                .into_os_string()
+                .into_string()
+                .expect("error parsing output string")
+        })
+        .unwrap_or("/tmp/backup.tar".to_string());
+    let files: Vec<PathBuf> = globs
+        .iter()
+        .flat_map(|g| glob::glob(g).unwrap())
+        .map(Result::unwrap)
+        .map(std::fs::canonicalize)
+        .map(Result::unwrap)
+        .collect();
+    run(Command::new("tar")
+        .args([
+            "--absolute-names",
+            "--transform=s|^|root|",
+            "-cf",
+            &output_file,
+        ])
+        .args(&files));
+    // TODO serialize backup.yml
+    run(Command::new("touch").arg("/tmp/backup.yml"));
+    run(Command::new("tar").args([
+        "--absolute-names",
+        "--transform=s|^.*$|backup.yml|",
+        "-rf",
+        &output_file,
+        "/tmp/backup.yml",
+    ]));
+    run(Command::new("gzip").arg("-f").arg(&output_file));
+    Ok(())
 }
 
 #[cfg(test)]
